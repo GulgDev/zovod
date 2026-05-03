b5fc48f7440884afe4c1f564a01465984cc4b948
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-03 18:46:24 +0500

    Update `Factory` class for the new map API

--- packages/engine/src/factory.ts
+++ packages/engine/src/factory.ts
@@ -1,37 +1,18 @@
-import { FactoryUnit } from "./factory-unit";
-import { Flow } from "./flow";
 import { Renderer } from "./render";
-
-function serializePos(x: number, y: number): number {
-  return (y << 16) | x;
-}
+import { FactoryMap } from "./map";
 
 export class Factory {
-  private unassignedWorkforceUnits: number = 0;
+  readonly map: FactoryMap = new FactoryMap();
 
-  private readonly units: Map<number, FactoryUnit> = new Map();
-  private readonly flows: Map<number, Flow> = new Map();
+  private unassignedWorkforceUnits: number = 0;
 
   constructor(private money: number) {}
 
-  placeUnit(unit: FactoryUnit, x: number, y: number): boolean {
-    const key = serializePos(x, y);
-    if (this.units.has(key)) return false;
-    this.units.set(key, unit);
-    return true;
-  }
-
-  removeUnit(x: number, y: number): void {
-    this.units.delete(serializePos(x, y));
-  }
-
   update(): void {
-    this.units.forEach((unit) => unit.update());
-    this.flows.forEach((flow) => flow.update());
+    for (const unit of this.map.getAllUnits()) unit.update();
   }
 
   render(renderer: Renderer): void {
-    this.units.forEach((unit) => unit.render(renderer));
-    this.flows.forEach((flow) => flow.render(renderer));
+    for (const unit of this.map.getAllUnits()) unit.render(renderer);
   }
 }
