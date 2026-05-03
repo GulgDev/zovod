e59d895b58daf8e4f4cd901837703e6bad8ccff4
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-04 00:51:42 +0500

    Fix negative coord packing

--- packages/engine/src/map/util/math.ts
+++ packages/engine/src/map/util/math.ts
@@ -1,14 +1,17 @@
 export type Point = readonly [number, number];
 
 export function packCoords(x: number, y: number): number {
-  if ((x & 0xffff) !== x || (y & 0xffff) !== y)
+  if ((x << 16) >> 16 !== x || (y << 16) >> 16 !== y)
     throw new Error(`Coordinates out of bounds (${x}, ${y})`);
 
-  return (y << 16) | x;
+  return (y << 16) | (x & 0xffff);
 }
 
 export function unpackCoords(key: number): Point {
-  return [key & 0xffff, key >> 16];
+  return [
+    (key << 16) >> 16, // shift left 16 bits to discard 16 highest bits
+    key >> 16,
+  ];
 }
 
 export function dist(x1: number, y1: number, x2: number, y2: number): number {
