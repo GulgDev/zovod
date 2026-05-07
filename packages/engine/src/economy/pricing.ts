6cef84f77e628367ef00e033067750a5863bc171
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-08 02:59:03 +0500

    Allow selling workforce

--- packages/engine/src/economy/pricing.ts
+++ packages/engine/src/economy/pricing.ts
@@ -1,6 +1,11 @@
 import { ResourceKind } from "../resource-kind";
 
+export interface Price {
+  readonly buy: number;
+  readonly sell: number;
+}
+
 export interface Pricing {
-  readonly [key: ResourceKind]: { buy: number; sell: number };
-  readonly workforceUnit: { buy: number };
+  readonly [key: ResourceKind]: Price;
+  readonly workforceUnit: Price;
 }
