0048a80b89544f16f6c3973a7b169d3f3670ff6d
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-09 02:11:39 +0500

    Refactor tests

---
+++ packages/engine/test/util/dummy-unit.ts
@@ -0,0 +1,15 @@
+import { FactoryUnit } from "../../src/factory/factory-unit";
+
+export class DummyUnit extends FactoryUnit {
+  protected canAccept(): boolean {
+    throw new Error("Not implemented");
+  }
+
+  protected accept(): void {
+    throw new Error("Not implemented");
+  }
+
+  protected doUpdate(): void {
+    throw new Error("Not implemented");
+  }
+}
