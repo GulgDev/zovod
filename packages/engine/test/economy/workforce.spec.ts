0f1ff7a84106f4223f57f89e4ce07aa0b68b2d70
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-10 05:13:09 +0500

    Add tests for `buyWorkforce` & `sellWorkforce`

--- packages/engine/test/economy/workforce.spec.ts
+++ packages/engine/test/economy/workforce.spec.ts
@@ -1,5 +1,57 @@
-import { describe } from "vitest";
+import { describe, expect, it } from "vitest";
+import { Inventory } from "../../src/economy/inventory";
 
 describe("Inventory - workforce", () => {
-  // TODO
+  describe("buyWorkforce", () => {
+    it("spends money and adds the corresponding amount of workforce units", () => {
+      const inventory = new Inventory(250, {
+        workforceUnit: { buy: 100, sell: 0 },
+      });
+
+      expect(inventory.buyWorkforce(2)).toBeTrue();
+
+      expect(inventory.totalWorkforce).toBe(2);
+      expect(inventory.unassignedWorkforce).toBe(2);
+      expect(inventory.balance).toBe(50);
+    });
+
+    it("fails when there is not enough money", () => {
+      const inventory = new Inventory(250, {
+        workforceUnit: { buy: 100, sell: 0 },
+      });
+
+      expect(inventory.buyWorkforce(3)).toBeFalse();
+
+      expect(inventory.totalWorkforce).toBe(0);
+      expect(inventory.unassignedWorkforce).toBe(0);
+      expect(inventory.balance).toBe(250);
+    });
+  });
+
+  describe("sellWorkforce", () => {
+    it("earns money", () => {
+      const inventory = new Inventory(0, {
+        workforceUnit: { buy: 0, sell: 100 },
+      });
+      inventory.buyWorkforce(2);
+
+      expect(() => inventory.sellWorkforce(2)).not.toThrow(
+        "Not enough unassigned workers to sell",
+      );
+
+      expect(inventory.totalWorkforce).toBe(0);
+      expect(inventory.unassignedWorkforce).toBe(0);
+      expect(inventory.balance).toBe(200);
+    });
+
+    it("throws when there is not enough unassigned workforce", () => {
+      const inventory = new Inventory(0, {
+        workforceUnit: { buy: 100, sell: 0 },
+      });
+
+      expect(() => inventory.sellWorkforce(1)).toThrow(
+        "Not enough unassigned workers to sell",
+      );
+    });
+  });
 });
