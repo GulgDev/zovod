f5f2d85e0a30e37c2b1f39a4cf5a200885fb6e89
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-10 17:04:31 +0500

    Add unique IDs to dummy units because `jest-extended` matchers test structural equality and not referential

--- packages/engine/test/factory-map/flows.spec.ts
+++ packages/engine/test/factory-map/flows.spec.ts
@@ -36,7 +36,7 @@ describe("FactoryMap - flow map", () => {
 
     it("fails when the flow segment has empty source or target", () => {
       const map = new FactoryMap();
-      map.placeUnit(new DummyUnit(), 0, 0);
+      map.placeUnit(new DummyUnit(1), 0, 0);
 
       expect(
         map.addFlowSegment([
@@ -59,7 +59,7 @@ describe("FactoryMap - flow map", () => {
 
     it("fails when the flow segment crosses an existing flow", () => {
       const map = new FactoryMap();
-      map.placeUnit(new DummyUnit(), 0, 0);
+      map.placeUnit(new DummyUnit(1), 0, 0);
 
       map.addFlowSegment([
         [0, 0],
@@ -80,7 +80,7 @@ describe("FactoryMap - flow map", () => {
 
     it("throws when the flow segment target is not a unit cell", () => {
       const map = new FactoryMap();
-      map.placeUnit(new DummyUnit(), 0, 0);
+      map.placeUnit(new DummyUnit(1), 0, 0);
 
       expect(() =>
         map.addFlowSegment([
@@ -112,9 +112,9 @@ describe("FactoryMap - flow map", () => {
 
     it("adds corresponding targets to the distribution", () => {
       const map = new FactoryMap();
-      const unit1 = new DummyUnit(),
-        unit2 = new DummyUnit(),
-        unit3 = new DummyUnit();
+      const unit1 = new DummyUnit(1),
+        unit2 = new DummyUnit(2),
+        unit3 = new DummyUnit(3);
       map.placeUnit(unit1, 0, 0);
       map.placeUnit(unit2, 2, 1);
       map.placeUnit(unit3, 2, -1);
@@ -145,8 +145,8 @@ describe("FactoryMap - flow map", () => {
       ["the end", 2, 1],
     ])("deletes a flow segment at %s", (_, x, y) => {
       const map = new FactoryMap();
-      map.placeUnit(new DummyUnit(), 0, 0);
-      map.placeUnit(new DummyUnit(), 2, 1);
+      map.placeUnit(new DummyUnit(1), 0, 0);
+      map.placeUnit(new DummyUnit(2), 2, 1);
       map.addFlowSegment([
         [0, 0],
         [1, 0],
@@ -169,9 +169,9 @@ describe("FactoryMap - flow map", () => {
 
     it("removes corresponding targets from the distribution", () => {
       const map = new FactoryMap();
-      const unit1 = new DummyUnit(),
-        unit2 = new DummyUnit(),
-        unit3 = new DummyUnit();
+      const unit1 = new DummyUnit(1),
+        unit2 = new DummyUnit(2),
+        unit3 = new DummyUnit(3);
       map.placeUnit(unit1, 0, 0);
       map.placeUnit(unit2, 2, 1);
       map.placeUnit(unit3, 2, -1);
--- packages/engine/test/factory-map/units.spec.ts
+++ packages/engine/test/factory-map/units.spec.ts
@@ -27,8 +27,8 @@ describe("FactoryMap - unit map", () => {
     it("places and retrieves units", () => {
       const map = new FactoryMap();
 
-      const unit1 = new DummyUnit(),
-        unit2 = new DummyUnit();
+      const unit1 = new DummyUnit(1),
+        unit2 = new DummyUnit(2);
 
       expect(map.placeUnit(unit1, 0, 0)).toBeTrue();
       expect(map.getUnitAt(0, 0)).toBe(unit1);
@@ -42,15 +42,15 @@ describe("FactoryMap - unit map", () => {
     it("fails when placing on an occupied cell", () => {
       const map = new FactoryMap();
 
-      expect(map.placeUnit(new DummyUnit(), 0, 0)).toBeTrue();
-      expect(map.placeUnit(new DummyUnit(), 0, 0)).toBeFalse();
+      expect(map.placeUnit(new DummyUnit(1), 0, 0)).toBeTrue();
+      expect(map.placeUnit(new DummyUnit(2), 0, 0)).toBeFalse();
     });
 
     it("throws for invalid positions", () => {
       const map = new FactoryMap();
 
       invalidPositions.forEach(([x, y]) =>
-        expect(() => map.placeUnit(new DummyUnit(), x, y)).toThrow(
+        expect(() => map.placeUnit(new DummyUnit(1), x, y)).toThrow(
           "Invalid unit position",
         ),
       );
@@ -61,8 +61,8 @@ describe("FactoryMap - unit map", () => {
     it("removes units", () => {
       const map = new FactoryMap();
 
-      const unit1 = new DummyUnit(),
-        unit2 = new DummyUnit();
+      const unit1 = new DummyUnit(1),
+        unit2 = new DummyUnit(2);
       map.placeUnit(unit1, 0, 0);
       map.placeUnit(unit2, 2, 1);
 
@@ -76,7 +76,7 @@ describe("FactoryMap - unit map", () => {
 
     it("fails for empty cells", () => {
       const map = new FactoryMap();
-      map.placeUnit(new DummyUnit(), 0, 0);
+      map.placeUnit(new DummyUnit(1), 0, 0);
 
       expect(map.removeUnitAt(0, 0)).toBeTrue();
       expect(map.removeUnitAt(2, 1)).toBeFalse();
@@ -95,8 +95,8 @@ describe("FactoryMap - unit map", () => {
     it("retrieves all units", () => {
       const map = new FactoryMap();
 
-      const unit1 = new DummyUnit(),
-        unit2 = new DummyUnit();
+      const unit1 = new DummyUnit(1),
+        unit2 = new DummyUnit(2);
       map.placeUnit(unit1, 0, 0);
       map.placeUnit(unit2, 2, 1);
 
