fb626177e400cf61a3664797338a813eb84c06dd
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-12 01:32:45 +0500

    Document economy-related classes and types

--- packages/engine/src/economy/pricing.ts
+++ packages/engine/src/economy/pricing.ts
@@ -1,10 +1,20 @@
 import type { ResourceKind } from "../resource-kind";
 
+/**
+ * An interface representing the purchase price and the sell price of a
+ * purchasable unit.
+ *
+ * @see {@link Pricing} for primary usage.
+ */
 export interface Price {
   readonly buy: number;
   readonly sell: number;
 }
 
+/**
+ * A record containing the purchase/sell prices of workforce units and all
+ * resources.
+ */
 export interface Pricing {
   readonly [key: ResourceKind]: Price;
   readonly workforceUnit: Price;
