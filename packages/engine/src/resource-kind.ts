aadded2fd7b57434e0632ba5fa0a3ff4a53e37be
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-11 23:12:11 +0500

    Document the `ResourceKind` type

--- packages/engine/src/resource-kind.ts
+++ packages/engine/src/resource-kind.ts
@@ -1,1 +1,5 @@
+/**
+ * A type representing the kind of a resource. All resource kinds are purely
+ * nominalistic and are meant to diversify the production process.
+ */
 export type ResourceKind = string;
