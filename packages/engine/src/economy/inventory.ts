6bd00e0510a4d7ea2d0eb45becaa69b986e76b31
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-12 01:32:45 +0500

    Document economy-related classes and types

--- packages/engine/src/economy/inventory.ts
+++ packages/engine/src/economy/inventory.ts
@@ -2,7 +2,16 @@ import type { ProductionPlant } from "../factory-units/production-plant";
 import type { ResourceKind } from "../resource-kind";
 import type { Pricing } from "./pricing";
 
+/**
+ * A class that stores player's money and workforce unit amount, controls all
+ * purchases/sales, and assigns/unassigns workforce from {@link ProductionPlant}
+ * factory units.
+ */
 export class Inventory {
+  /**
+   * @param currentBalance - Initial amount of money on the balance.
+   * @param pricing - A record containing purchase/sell prices of workforce units and all resources.
+   */
   constructor(
     private currentBalance: number,
     private readonly pricing: Pricing,
@@ -12,62 +21,135 @@ export class Inventory {
     return this.currentBalance;
   }
 
+  /**
+   * Calculate the maximum amount of a certain unit that can be purchased with
+   * the current {@link balance}.
+   *
+   * @param price - The price of the purchasable unit.
+   * @returns An integer representing the maximum affordable amount of the unit.
+   */
   private getMaxAffordableAmount(price: number): number {
     return Math.floor(this.currentBalance / price);
   }
 
+  /**
+   * Try to remove the given amount of money from the current {@link balance}.
+   *
+   * @returns `false` if there is not enough money, otherwise `true` if the
+   * operation was successful.
+   */
   spendMoney(amount: number): boolean {
     if (this.currentBalance < amount) return false;
     this.currentBalance -= amount;
     return true;
   }
 
+  /**
+   * Add the given amount of money to the current {@link balance}.
+   */
   earnMoney(amount: number): void {
     this.currentBalance += amount;
   }
 
-  getResourcePrice(resource: ResourceKind): number {
-    return this.pricing[resource].buy;
+  /**
+   * Retrieve the purchase price of a resource from the {@link Pricing}
+   * specified at the construction time.
+   */
+  getResourcePrice(resourceKind: ResourceKind): number {
+    return this.pricing[resourceKind].buy;
   }
 
-  getResourceSellPrice(resource: ResourceKind): number {
-    return this.pricing[resource].sell;
+  /**
+   * Retrieve the sell price of a resource from the {@link Pricing} specified
+   * at the construction time.
+   */
+  getResourceSellPrice(resourceKind: ResourceKind): number {
+    return this.pricing[resourceKind].sell;
   }
 
-  getMaxAffordableResourceAmount(resource: ResourceKind): number {
-    return this.getMaxAffordableAmount(this.pricing[resource].buy);
+  /**
+   * Calculate the maximum amount of resources that can be purchased with the
+   * current {@link balance} using the {@link Pricing} specified at the
+   * construction time.
+   *
+   * @returns An integer representing the maximum affordable amount of the resource.
+   */
+  getMaxAffordableResourceAmount(resourceKind: ResourceKind): number {
+    return this.getMaxAffordableAmount(this.pricing[resourceKind].buy);
   }
 
+  /**
+   * Try to remove the total price of the specified resource kind from the
+   * current {@link balance}, given the amount.
+   *
+   * @returns `false` if there is not enough money, otherwise `true` if the
+   * operation was successful.
+   */
   buyResource(resource: ResourceKind, amount = 1): boolean {
     return this.spendMoney(this.pricing[resource].buy * amount);
   }
 
+  /**
+   * Add the total price of the specified resource kind to the current
+   * {@link balance}, given the amount.
+   */
   sellResource(resource: ResourceKind, amount = 1): void {
     this.earnMoney(this.pricing[resource].sell * amount);
   }
 
+  /**
+   * Retrieve the purchase price of a workforce unit from the {@link Pricing}
+   * specified at the construction time.
+   */
   getWorkforceUnitPrice(): number {
     return this.pricing.workforceUnit.buy;
   }
 
+  /**
+   * Retrieve the sell price of a workforce unit from the {@link Pricing}
+   * specified at the construction time.
+   */
   getWorkforceUnitSellPrice(): number {
     return this.pricing.workforceUnit.sell;
   }
 
+  /**
+   * Calculate the maximum amount of workforce that can be purchased with the
+   * current {@link balance} using the {@link Pricing} specified at the
+   * construction time.
+   *
+   * @returns An integer representing the maximum affordable number of workforce units.
+   */
   getMaxAffordableWorkforceUnits(): number {
     return this.getMaxAffordableAmount(this.pricing.workforceUnit.buy);
   }
 
-  private currentTotalWorkforce = 0;
+  /** The total number of workforce units ever purchased. */
   get totalWorkforce(): number {
     return this.currentTotalWorkforce;
   }
+  private currentTotalWorkforce = 0;
 
-  private currentUnassignedWorkforce = 0;
+  /**
+   * The number of available workforce units that are not assigned to a
+   * {@link ProductionPlant}.
+   *
+   * @see {@link assignWorkforce}
+   * @see {@link unassignWorkforce}
+   */
   get unassignedWorkforce(): number {
     return this.currentUnassignedWorkforce;
   }
+  private currentUnassignedWorkforce = 0;
 
+  /**
+   * Try to remove the total price of the given amount of workforce units from
+   * the current {@link balance}, and add corresponding number of workforce
+   * units to {@link totalWorkforce} upon success.
+   *
+   * @returns `false` if there is not enough money, otherwise `true` if the
+   * operation was successful.
+   */
   buyWorkforce(units: number): boolean {
     const canAfford = this.spendMoney(this.pricing.workforceUnit.buy * units);
     if (canAfford) {
@@ -77,6 +159,13 @@ export class Inventory {
     return canAfford;
   }
 
+  /**
+   * Add the total price of the given amount of workforce units to the current
+   * {@link balance}, and remove the corresponding number of unassigned
+   * workforce units from {@link totalWorkforce}.
+   *
+   * @throws Will throw an error if {@link unassignedWorkforce} is less than the specified amount of workforce units.
+   */
   sellWorkforce(units: number): void {
     if (this.currentUnassignedWorkforce < units)
       throw new Error("Not enough unassigned workers to sell");
@@ -86,6 +175,15 @@ export class Inventory {
     this.earnMoney(this.pricing.workforceUnit.sell * units);
   }
 
+  /**
+   * Assign the given amount of workforce units to the specified
+   * {@link ProductionPlant}. The assigned workforce must be unassigned to
+   * become available again.
+   *
+   * @throws Will throw an error if {@link unassignedWorkforce} is less than the specified amount of workforce units.
+   *
+   * @see {@link unassignWorkforce}
+   */
   assignWorkforce(productionPlant: ProductionPlant, units: number): void {
     if (this.currentUnassignedWorkforce < units)
       throw new Error("Not enough unassigned workers");
@@ -97,6 +195,14 @@ export class Inventory {
     );
   }
 
+  /**
+   * Unassign the given amount of workforce units from the specified
+   * {@link ProductionPlant}.
+   *
+   * @throws Will throw an error if the {@link ProductionPlant} has less workforce units assigned than the specified amount.
+   *
+   * @see {@link assignWorkforce}
+   */
   unassignWorkforce(productionPlant: ProductionPlant, units: number): void {
     const plantAssignedWorkforce =
       Inventory.getAssignedWorkforce(productionPlant);
