fe720971ba1d8326877fa9d3a27db907e3f88049
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-16 00:57:27 +0500

    Document `FactoryUnit` abstract class

--- packages/engine/src/factory-units/abstract/factory-unit.ts
+++ packages/engine/src/factory-units/abstract/factory-unit.ts
@@ -3,11 +3,38 @@ import type { ResourceKind } from "../../resource-kind";
 import { FactoryMap } from "../../factory-map";
 import { sampleFrom } from "../util/sample";
 
+// Types linked to in JSDoc
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Market } from "../market";
+import type { ProductionPlant } from "../production-plant";
+import type { Storage } from "../storage";
+/* eslint-ensable @typescript-eslint/no-unused-vars */
+
+/**
+ * An abstract class for factory units that handles the send/accept mechanism
+ * and update logic.
+ *
+ * Factory units are a primary game element, which, together with flows, forms
+ * a factory, i.e. a pipeline that buys, transforms and sells resources.
+ *
+ * @see {@link Market}, {@link ProductionPlant}, and {@link Storage} for concrete factory unit types.
+ */
 export abstract class FactoryUnit {
+  /**
+   * Check whether a resource can be accepted by the unit.
+   */
   protected abstract canAccept(resource: ResourceKind): boolean;
 
+  /**
+   * Accept the resource and perform an action with it (e.g. store, sell, etc.).
+   */
   protected abstract accept(resource: ResourceKind): void;
 
+  /**
+   * Send a resource to a randomly chosen target unit.
+   *
+   * @see {@link FactoryMap.setTargetDistribution} for more information on the target distribution.
+   */
   protected send(resource: ResourceKind): boolean {
     const distribution = new Map(
       FactoryMap.getTargetDistribution(this)
@@ -23,11 +50,25 @@ export abstract class FactoryUnit {
 
   protected abstract doUpdate(game: Game, deltaTime: number): void;
 
+  /**
+   * Determine whether the factory unit is currently paused and does not accept
+   * resources, perform simulation steps and any other actions.
+   */
   paused = false;
 
+  /**
+   * Perform a simulation step on the factory unit. If {@link paused} is set to
+   * true, does nothing.
+   *
+   * @param game - The current active {@link Game} instance, with which the factory unit is associated.
+   * @param deltaTime - Time (in seconds) passed since the last game update.
+   */
   update(game: Game, deltaTime: number): void {
     if (!this.paused) this.doUpdate(game, deltaTime);
   }
 
+  /**
+   * Check the state of the factory unit and throw if it cannot be removed.
+   */
   remove?(): void;
 }
