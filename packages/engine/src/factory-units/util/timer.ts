6dc345a9492b9ecd02a5ef052bbc1757d521cd24
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-16 01:21:58 +0500

    Document `Timer` class

--- packages/engine/src/factory-units/util/timer.ts
+++ packages/engine/src/factory-units/util/timer.ts
@@ -1,14 +1,32 @@
+/**
+ * An utility class that measures in-game time to trigger repeating or delayed
+ * actions.
+ *
+ * @internal
+ */
 export class Timer {
+  /**
+   * @param time - The time in seconds after which the timer will set off.
+   */
   constructor(private time = 0) {}
 
   get expired(): boolean {
     return this.time <= 0;
   }
 
+  /**
+   * @param time - The time in seconds after which the timer will set off after reset.
+   */
   reset(time: number): void {
     this.time = time;
   }
 
+  /**
+   * Advance the timer.
+   *
+   * @param deltaTime - The amount of time (in seconds) to advance for.
+   * @returns `true` if the timer set off during the current advance, `false` otherwise.
+   */
   update(deltaTime: number): boolean {
     return this.time > 0 && (this.time -= deltaTime) <= 0;
   }
