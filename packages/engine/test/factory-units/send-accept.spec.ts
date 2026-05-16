96dc488e61a6571df251985bc7de915a8b1e803a
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-16 19:55:04 +0500

    Add tests for `FactoryUnit.sendOneOf`

--- packages/engine/test/factory-units/send-accept.spec.ts
+++ packages/engine/test/factory-units/send-accept.spec.ts
@@ -1,3 +1,4 @@
+import { when } from "jest-when";
 import { describe, expect, it, vi } from "vitest";
 import { UnitMock } from "../util/unit-mock";
 import type { ResourceKind } from "../../src/resource-kind";
@@ -5,53 +6,97 @@ import { FactoryMap } from "../../src/factory-map";
 
 vi.mock("../../src/factory-map");
 
-const resource: ResourceKind = "resource";
+const resourceA: ResourceKind = "a",
+  resourceB: ResourceKind = "b";
 
 describe("Factory units - send/accept", () => {
-  it("returns false when there are no targets", () => {
-    const sender = new UnitMock();
+  describe("send", () => {
+    it("returns false when there are no targets", () => {
+      const sender = new UnitMock();
 
-    vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(new Map());
+      vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
+        new Map(),
+      );
 
-    expect(sender.send(resource)).toBeFalse();
+      expect(sender.send(resourceA)).toBeFalse();
+    });
+
+    it("does not send resources to targets that cannot accept it", () => {
+      const sender = new UnitMock(),
+        target = new UnitMock();
+
+      vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
+        new Map([[target, 1]]),
+      );
+      target.canAccept.mockReturnValueOnce(false);
+
+      expect(sender.send(resourceA)).toBeFalse();
+    });
+
+    it("does not send resources to targets that are paused", () => {
+      const sender = new UnitMock(),
+        target = new UnitMock();
+      target.paused = true;
+
+      vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
+        new Map([[target, 1]]),
+      );
+      target.canAccept.mockReturnValueOnce(true);
+
+      expect(sender.send(resourceA)).toBeFalse();
+    });
+
+    it("calls the `accept` method of the target", () => {
+      const sender = new UnitMock(),
+        target = new UnitMock();
+
+      vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
+        new Map([[target, 1]]),
+      );
+      target.canAccept.mockReturnValueOnce(true);
+
+      expect(sender.send(resourceA)).toBeTrue();
+
+      expect(target.accept).toHaveBeenCalledExactlyOnceWith(resourceA);
+    });
   });
 
-  it("does not send resources to targets that cannot accept it", () => {
-    const sender = new UnitMock(),
-      target = new UnitMock();
+  describe("sendOneOf", () => {
+    it("returns undefined when there are no targets", () => {
+      const sender = new UnitMock();
 
-    vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
-      new Map([[target, 1]]),
-    );
-    target.canAccept.mockReturnValueOnce(false);
+      vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
+        new Map(),
+      );
 
-    expect(sender.send(resource)).toBeFalse();
-  });
+      expect(sender.sendOneOf(new Set(resourceA))).toBeUndefined();
+    });
 
-  it("does not send resources to targets that are paused", () => {
-    const sender = new UnitMock(),
-      target = new UnitMock();
-    target.paused = true;
+    it("sends the resource with the most accepting targets", () => {
+      const sender = new UnitMock();
 
-    vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
-      new Map([[target, 1]]),
-    );
-    target.canAccept.mockReturnValueOnce(true);
+      const target1 = new UnitMock();
+      when(target1.canAccept).calledWith(resourceA).mockReturnValue(true);
+      when(target1.canAccept).calledWith(resourceB).mockReturnValue(false);
 
-    expect(sender.send(resource)).toBeFalse();
-  });
+      const target2 = new UnitMock();
+      when(target2.canAccept).calledWith(resourceA).mockReturnValue(true);
+      when(target2.canAccept).calledWith(resourceB).mockReturnValue(false);
 
-  it("calls the `accept` method of the target", () => {
-    const sender = new UnitMock(),
-      target = new UnitMock();
+      const target3 = new UnitMock();
+      when(target3.canAccept).calledWith(resourceA).mockReturnValue(false);
+      when(target3.canAccept).calledWith(resourceB).mockReturnValue(true);
 
-    vi.mocked(FactoryMap.getTargetDistribution).mockReturnValueOnce(
-      new Map([[target, 1]]),
-    );
-    target.canAccept.mockReturnValueOnce(true);
+      vi.mocked(FactoryMap.getTargetDistribution).mockReturnValue(
+        new Map([
+          [target1, 0.5],
+          [target2, 0],
+          [target3, 0.5],
+        ]),
+      );
 
-    expect(sender.send(resource)).toBeTrue();
-
-    expect(target.accept).toHaveBeenCalledExactlyOnceWith(resource);
+      expect(sender.sendOneOf(new Set([resourceA, resourceB]))).toBe(resourceA);
+      expect(target1.accept).toHaveBeenCalledExactlyOnceWith(resourceA);
+    });
   });
 });
