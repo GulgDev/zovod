3cfc0620001550d0c8d62c55ed519f45ec09bef2
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-05-16 20:38:33 +0500

    Reset all `jest-when` mocks before each test

--- test-setup.ts
+++ test-setup.ts
@@ -1,7 +1,12 @@
-import { expect } from "vitest";
+import { beforeEach, expect } from "vitest";
 import * as matchers from "jest-extended";
+import { resetAllWhenMocks } from "jest-when";
 
 expect.extend(matchers);
+
+beforeEach(() => {
+  resetAllWhenMocks();
+});
 
 import "core-js/es/map/get-or-insert-computed";
 import "core-js/es/weak-map/get-or-insert-computed";
