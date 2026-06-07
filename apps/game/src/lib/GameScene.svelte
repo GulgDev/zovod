c65d1709ef49e82fc907fbb31d552da61f173354
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-06-08 00:05:41 +0500

    Style background

--- apps/game/src/lib/GameScene.svelte
+++ apps/game/src/lib/GameScene.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  const TILE_SIZE = 1,
-    TILE_GAP = 0.45;
+  // Sizes and offsets
+  const TILE_SIZE = 1 / 8,
+    TILE_GAP = TILE_SIZE * 0.45;
 
   // Offset odd columns vertically so that the gaps connect the centers of
   // surrounding tiles:
@@ -12,12 +13,16 @@
   //         ######
   // ######  ######
   const ODD_COLUMN_Y_OFFSET = (TILE_SIZE + TILE_GAP) / 2;
+
+  // Border parameters
+  const TILE_BORDER_SIZE = TILE_SIZE * 0.035,
+    TILE_BORDER_RADIUS = TILE_SIZE * 0.07;
 </script>
 
 <svg
   width="100%"
   height="100%"
-  viewBox="0 0 {8 * TILE_SIZE} {8 * TILE_SIZE}"
+  viewBox="0 0 1 1"
   preserveAspectRatio="xMinYMin slice"
 >
   <defs>
@@ -30,7 +35,29 @@
       patternUnits="userSpaceOnUse"
     >
       <defs>
-        <rect id="grid-tile" width={TILE_SIZE} height={TILE_SIZE} fill="red" />
+        <!--
+          SVG doesn't support inset borders, so we use a hack here: define a
+          shape, and use it as its own clip path, doubling the stroke width.
+        -->
+        <rect
+          id="grid-tile-shape"
+          width={TILE_SIZE}
+          height={TILE_SIZE}
+          rx={TILE_BORDER_RADIUS}
+          ry={TILE_BORDER_RADIUS}
+        />
+        <clipPath id="grid-tile-clip">
+          <use href="#grid-tile-shape" />
+        </clipPath>
+
+        <use
+          id="grid-tile"
+          href="#grid-tile-shape"
+          clip-path="url(#grid-tile-clip)"
+          fill="transparent"
+          stroke="hsla(41, 72%, 89%, 1)"
+          stroke-width={TILE_BORDER_SIZE * 2}
+        />
       </defs>
 
       <use href="#grid-tile" x="0" y="0"></use>
@@ -45,5 +72,6 @@
   </defs>
 
   <!-- Background -->
+  <rect fill="hsla(42, 100%, 97%, 1)" width="100%" height="100%" />
   <rect fill="url(#background-grid-pattern)" width="100%" height="100%" />
 </svg>
