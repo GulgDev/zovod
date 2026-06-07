6ddaf4d46d68bb1dbe70e778335726b2d600ea55
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-06-07 17:01:04 +0500

    Use `.js` extension for configs instead of `.mjs`

--- packages/engine/eslint.config.js
+++ packages/engine/eslint.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import base from "@zovod/eslint-config/base";
 
-/** @type {import("eslint").Linter.Config} */
 export default defineConfig(
   tseslint.config({
     languageOptions: {
