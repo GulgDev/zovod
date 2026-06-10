833785114eea8b2134577020b0856d52dd12453a
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-06-10 14:31:20 +0500

    Support legacy browsers

--- apps/game/vite.config.js
+++ apps/game/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), legacy()],
   base: process.env.VITE_BASE_PATH?.replace(/(?<!\/)$/, "/"), // add trailing slash
 });
